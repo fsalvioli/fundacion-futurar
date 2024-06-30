eeb904050c03ab88af70d07cd25aee9f04f9d985
FrancoSalvioli <salvioli.franco@gmail.com>
2024-06-30 20:14:39 -0300

    static changes

--- news_page/settings.py
+++ news_page/settings.py
@@ -164,10 +164,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+#STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
