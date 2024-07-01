6b904ec29ba28c0a47e6d8d0f5c75357394b0113
FrancoSalvioli <salvioli.franco@gmail.com>
2024-07-01 09:59:06 -0300

    static changes 2

--- news_page/settings.py
+++ news_page/settings.py
@@ -164,9 +164,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = "static/"
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, "static"),
-#]
+STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 #STATIC_ROOT = BASE_DIR / "staticfiles"
 
