ce29a724f0b766aeea821d30d6d62dcb7d1c66cd
FrancoSalvioli <salvioli.franco@gmail.com>
2024-07-01 10:33:00 -0300

    basedir

--- news_page/settings.py
+++ news_page/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
