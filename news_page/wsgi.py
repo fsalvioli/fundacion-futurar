736478e57d7fb54c6f644ec0adcff2f2d53d8cab
FrancoSalvioli <salvioli.franco@gmail.com>
2024-07-01 12:19:58 -0300

    others 2

--- news_page/wsgi.py
+++ news_page/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-import settings
+from django.conf import settings
 
 
 if os.environ.get('DJANGO_ENV') == 'production':
